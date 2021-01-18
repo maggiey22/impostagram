8cdb639452c26e9118d35891bbba4aa624fb185a
Maggie <43491401+maggiey22@users.noreply.github.com>
2021-01-17 18:03:55 -0800

    rename Navbar component

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -2,15 +2,17 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
+import logo from '../impostagram.png';
+
 export default function CustomNavbar() {
     return (
         <Navbar bg="light" variant="light" sticky="top" className="common">
             <Navbar.Brand href="/" className="logo">
                 <img
-                    src="/impostagram.png"
+                    src={logo}
                     height="35"
                     className="d-inline-block align-middle"
-                    alt="Impostgram logo"
+                    alt="Impostagram logo"
                 />
                 {'   '}
                 Impostagram
